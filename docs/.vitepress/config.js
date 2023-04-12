fbfd2ff48e5272ae2ead2fb76ea90487e6b3733d
ChunKam Yu <awa@outlook.ie>
2023-04-12 17:30:05 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -80,26 +80,6 @@ export default {
 //           placeholder: '请输入关键词',
 //           buttonText: '搜索',
 //         }
-    localSearch: {
-        enable: true,
-          locales: {
-            zh: {
-              translations: {
-                button: {
-                  buttonText: '搜索文档',
-                  buttonAriaLabel: '搜索文档'
-                },
-                modal: {
-                  noResultsText: '无法找到相关结果',
-                  resetButtonTitle: '清除查询条件',
-                  footer: {
-                    selectText: '选择',
-                    navigateText: '切换'
-                  }
-                }
-              }
-            }
-          }
-        }
+        localSearch: true,
     }
 }
