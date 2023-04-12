0be0ff5a3657d61dd406539f59c6b350e1b925b4
ChunKam Yu <awa@outlook.ie>
2023-04-12 17:15:31 +0800

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,17 @@
 import DefaultTheme from 'vitepress/theme'
-import theme from 'vitepress/theme'
+import {
+    theme,
+    VPHomeHero,
+    VPHomeFeatures,
+    VPHomeSponsors,
+    VPDocAsideSponsors,
+    VPTeamPage,
+    VPTeamPageTitle,
+    VPTeamPageSection,
+    VPTeamMembers,
+} from 'vitepress/theme'
 import Comment from "./comment.vue";
+
 export default {
     ...DefaultTheme,
     ...theme,
