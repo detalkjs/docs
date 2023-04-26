70ab674aaefadb7373c07544102891da4f0c5925
ChunKam Yu <awa@outlook.ie>
2023-04-26 12:15:41 +0800

    Update try-detalk.js

--- docs/public/try-detalk.js
+++ docs/public/try-detalk.js
@@ -1,3 +1,17 @@
+function blobToBase64(blob) {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = (e) => {
+        resolve(e.target.result);
+        };
+        // readAsDataURL
+        fileReader.readAsDataURL(blob);
+        fileReader.onerror = () => {
+        reject(new Error('blobToBase64 error'));
+        };
+    });
+}
+
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         if (window.detalk && document.getElementById("try_detalkjs_here")) {
